f7163c4af73dab397e4a41b9b45662adc93b84c8
ihjacobs925 <ihjacobs925@gmail.com>
2021-02-16 18:34:42 -0600

    GET routes plus create route

---
+++ controllers/users.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+// //Method 1
+// let topDog = new User({
+//     name: 'Anna Zocher',
+//     email: 'anna.zocher@ga.co'
+// });
+// topDog.save(err => {
+//     if (err) return console.error(`YIKES bud \n${err}`);
+//     console.log(`${topDog.name} was successfully created!`);
+// });
+
+// // Method 2
+// User.create({
+//     name: 'Gavin Callander',
+//     email: 'tosspot@Ga.co'
+// }, (err, user) => {
+//     if (err) return console.error(`YIKES Trouble in Create:\n${err}`);
+//     console.log(`${user.name} hath been created`);
+// })
+
+// Index - GET /users
+router.get('/', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) {
+            console.error(`ERROR in USERS INDEX ROUTE:\n${err}`);
+            res.status(500).json({ error: `ERROR in USERS index ROUTE`})
+        }
+        res.json({ users });
+    });
+});
+
+// Detail - GET /users/:id
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+        if (err) {
+            console.error(`ERROR in USERS DETAIL ROUTE:\n${err}`);
+            res.status(500).json({ error: `ERROR in USERS DETAILS ROUTE`})
+        }
+        res.json({ user });
+    });
+});
+
+// Create - POST
+router.post('/', (req, res) => {
+    console.log(req.body);
+    User.create(req.body, (err, user) => {
+        if (err) {
+            console.error(`ERROR in USERS Create ROUTE:\n${err}`);
+            res.status(500).json({ error: `ERROR in USERS Create ROUTE`})
+        }
+        res.json({ user });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -29,28 +29,31 @@ db.on('error', err => {
 //Testing Mongoose - TODO: Remove and fill with actual CRUD
 const User = require('./models/user');
 
-//Method 1
-let topDog = new User({
-    name: 'Anna Zocher',
-    email: 'anna.zocher@ga.co'
-});
-topDog.save(err => {
-    if (err) return console.error(`YIKES bud \n${err}`);
-    console.log(`${topDog.name} was successfully created!`);
-});
+// //Method 1
+// let topDog = new User({
+//     name: 'Anna Zocher',
+//     email: 'anna.zocher@ga.co'
+// });
+// topDog.save(err => {
+//     if (err) return console.error(`YIKES bud \n${err}`);
+//     console.log(`${topDog.name} was successfully created!`);
+// });
 
-// Method 2
-User.create({
-    name: 'Gavin Callander',
-    email: 'tosspot@GainNode.co'
-}, (err, user) => {
-    if (err) return console.error(`YIKES Trouble in Create:\n${err}`);
-    console.log(`${user.name} hath been created`);
-})
+// // Method 2
+// User.create({
+//     name: 'Gavin Callander',
+//     email: 'tosspot@GainNode.co'
+// }, (err, user) => {
+//     if (err) return console.error(`YIKES Trouble in Create:\n${err}`);
+//     console.log(`${user.name} hath been created`);
+// })
 
 // Route
 app.get('/', (req, res) => {
     res.json({message: 'Home'});
 });
+
+app.use('/users', require('./controllers/users'));
+
 
 app.listen(3000 || process.env.PORT, () => console.log(`You are listening to the smooth sounds of port ${3000 || process.env.PORT}`));
